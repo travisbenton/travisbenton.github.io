8e2bbe22de5a86b223b56513584850b23d6a7a0a
Travis Benton <travisryanbenton@gmail.com>
2017-01-17 22:14:09 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
-var duration = 10000;
+var duration = 5000;
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
-$('div').fadeOut(duration);
-setTimeout(function () {
-  window.location.href = 'http://www.reddeervillarvranch.com/';
+$('img').each(function() {
+  var $this = $(this);
+  setTimeout(function() {
+    $this.attr('src', 'http://reddeervillarvranch.com/animation_clear.gif');
+  }, getRandomArbitrary(0, duration));
+});
+
+setTimeout(function() {
+  $('div').fadeOut(duration);
+  setTimeout(function () {
+    window.location.href = 'http://www.reddeervillarvranch.com/';
+  }, duration);
 }, duration);
