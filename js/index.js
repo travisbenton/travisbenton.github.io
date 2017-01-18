bdd2e3fc1c120cd5f6452bbec19c2aa1220b8e4c
Travis Benton <travisryanbenton@gmail.com>
2017-01-17 22:29:11 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,23 +1,26 @@
-var duration = 5000;
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
-}
+if (!window.MusicMan) {
+  var duration = 5000;
+  function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
 
-$(window).on("load", function() {
-  setTimeout(function () {
-    $('img').each(function() {
-      var $this = $(this);
+  $(window).on("load", function() {
+    setTimeout(function () {
+      $('img').each(function() {
+        var $this = $(this);
+        setTimeout(function() {
+          $this.attr('src', Math.random() > .5 ? 'http://reddeervillarvranch.com/animation_clear.gif' : 'http://www.acountrybreezervpark.com/pet%20image.gif');
+        }, getRandomArbitrary(0, duration));
+      });
+
       setTimeout(function() {
-        if ($this.attr('src') === 'http://reddeervillarvranch.com/animation_clear.gif' || 'http://www.acountrybreezervpark.com/pet%20image.gif') return;
-        $this.attr('src', Math.random() > .5 ? 'http://reddeervillarvranch.com/animation_clear.gif' : 'http://www.acountrybreezervpark.com/pet%20image.gif');
-      }, getRandomArbitrary(0, duration));
-    });
+        $('div').fadeOut(duration);
+        setTimeout(function () {
+          window.location.href = 'http://www.acountrybreezervpark.com/';
+        }, duration/2);
+      }, duration);
+    }, duration/2);
+  });
 
-    setTimeout(function() {
-      $('div').fadeOut(duration);
-      setTimeout(function () {
-        window.location.href = 'http://www.acountrybreezervpark.com/';
-      }, duration/2);
-    }, duration);
-  }, duration/2);
-});
+  window.MusicMan = true;
+}
